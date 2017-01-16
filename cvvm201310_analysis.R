b971a14d32acd3daf03cf0dd4e614cc587c82feb
krojtous <matousp@gmail.com>
2017-01-16 15:34:26 +0100

    update

---
+++ cvvm201310_analysis.R
@@ -0,0 +1,111 @@
+#Date: 15.1.2017
+#Author: Matouš Pilnáček - Public Opinion Research Centre, Czech Academy of Science
+#E-mail: matous.pilnacek@soc.cas.cz
+#Description: Basic analysis of survey conducted before czech parliamentary elections 2013(lower house)
+#Data source: http://nesstar.soc.cas.cz/webview/velocity?mode=download&analysismode=table&v=2&study=http%3A%2F%2F147.231.52.118%3A80%2Fobj%2FfStudy%2FV1310
+
+
+
+library(foreign)
+cvvm = read.spss(file = "./data/V1310/V1310_F1.sav",
+                 to.data.frame = TRUE,
+                 use.value.labels = FALSE,
+                 use.missings = FALSE)
+
+#
+#RESULTS - http://www.volby.cz/pls/ps2013/ps2?xjazyk=CZ
+results = read.csv(file = "./results.csv",
+                   sep = ";",
+                   dec = ",")
+turnout = 59.48
+#RECODE PV_4 (dont know - 0,98,99 to missing)
+cvvm$PV_4R = cvvm$PV_4
+cvvm[cvvm$PV_4 %in% c(0,98,99), "PV_4R"] = NA
+
+#PV4 ANALYSIS
+tableRel = function(data, variable){
+  table = as.data.frame(table(data[,variable]))
+  table = cbind(table, round( table$Freq/sum(table$Freq)*100,2 ))
+  names(table) = c(variable,"abs","rel")
+  return(table)
+}
+
+PV4 = tableRel(cvvm, "PV_4R")
+
+#limit for party in percents
+limit = 3
+
+#RECODE PV_4R (small parties to missing/one category)
+others = as.vector(PV4[PV4$rel < limit, "PV_4R"])
+cvvm[cvvm$PV_4 %in% others, "PV_4R"] = NA
+#cvvm[cvvm$PV_4 %in% others, "PV_4R"] = "97"
+
+PV4R = tableRel(cvvm, "PV_4R")
+
+#extract labels of parties
+labels = as.data.frame(cbind(attr(cvvm$PV_4, "value.labels"), names(attr(cvvm$PV_4, "value.labels"))))
+names(labels) = c("PV_4R", "label")
+
+PV4R = merge(labels, PV4R)
+
+#order by party number - important for further merging by cbind
+PV4R = PV4R[order(as.numeric(as.vector(PV4R$PV_4R))),]
+
+
+#-------------position of voters in 2D SPACE-----------------
+
+# #RECODE FIRST VARIABLE
+# table(cvvm$OV_1)
+# cvvm$OV_1R = cvvm$OV_1
+# cvvm[cvvm$OV_1R %in% c(0,9) , "OV_1R"] = NA
+# 
+# #COMPUTE FIRST VARIABLE
+# agreg = aggregate(cvvm$OV_1R, by = list(as.vector(cvvm$PV_4R)), mean, na.rm = TRUE)
+# PV4R = cbind(PV4R, agreg[-1])          
+
+#RECODE FIRST VARIABLE
+table(cvvm$OV_132)
+cvvm$OV_132R = cvvm$OV_132
+cvvm[cvvm$OV_132R %in% c(0,9) , "OV_132R"] = NA
+
+#COMPUTE FIRST VARIABLE
+agreg = aggregate(cvvm$OV_132R, by = list(as.vector(cvvm$PV_4R)), mean, na.rm = TRUE)
+PV4R = cbind(PV4R, agreg[-1]) 
+
+
+#RECODE SECOND VARIABLE
+table(cvvm$PO_2)
+cvvm$PO_2R = cvvm$PO_2
+cvvm[cvvm$PO_2R %in% c(0,99) , "PO_2R"] = NA
+
+#COMPUTE SECOND VARIABLE
+agreg2 = aggregate(cvvm$PO_2R, by = list(cvvm$PV_4R), mean, na.rm = TRUE)
+PV4R = cbind(PV4R, agreg2[-1])
+
+names(PV4R) = c("number","party","abs","rel","x","y")
+
+#----------------------------DRAW GRAPH----------------------------
+library(ggplot2)
+
+# ggplot(PV4R, aes(y, x, label = label) ) +
+#   geom_point(aes(size = rel), colour = "blue") +
+#   geom_text(aes(lineheight = 0.8), size=3, vjust = -1)
+
+library(plotly)
+p <- plot_ly(midwest, x = ~percollege, color = ~state, type = "box")
+p
+
+colors <- c('rgba(255,128,0,1)', 'rgba(0,0,0,1)', 'rgba(127,0,255,1)', 'rgba(0,0,204,1)', 'rgba(255,255,0,1)',
+            'rgba(204,204,204,1)', 'rgba(204,204,204,1)', 'rgba(153,153,255,1)', 'rgba(255,0,0,1)')
+
+p <- plot_ly(PV4R, x = ~y, y = ~x, type = 'scatter', mode = 'markers',
+             marker = list(size = ~rel*2, opacity = 1, color = colors),
+             hoverinfo = 'text',
+             text = ~paste('Strana:', party, '<br>Procento:', rel, '<br>N:', abs)) %>%
+  layout(title = 'Rozložení stran - říjen 2013',
+         xaxis = list(showgrid = FALSE, title = "Pravo-levé sebezaření (PO.2)"),
+         yaxis = list(showgrid = FALSE, title = "Hrdost být občanem ČR (OV.132)"))
+p
+
+#PV93
+table(cvvm$PV_93)
